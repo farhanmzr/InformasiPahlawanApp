f87a5c7dba5bba4bac63d152580f2400af776d6d
Farhan Mazario <38252254+farhanmzr@users.noreply.github.com>
2019-12-28 03:55:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myrecyclerview/Hero.java
@@ -0,0 +1,31 @@
+package com.example.myrecyclerview;
+
+public class Hero {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+
+    private String name;
+    private String detail;
+    private int photo;
+}
